9b5e1de3e57efdb4563a92eec6048106dbea8ab5
Mateusz <mateuszkdev@users.noreply.github.com>
2021-01-27 23:14:56 +0100

    first look of projects

--- src/private/endpoints/projects.ts
+++ src/private/endpoints/projects.ts
@@ -1,16 +1,15 @@
 import { endPointInterface } from '../types/endpointInterface'
-const { socialmedia } = require('../../../settings/media.json')
+const { projects } = require('../../../settings/projects.json')
 const lang = require('../../../settings/lang.json')
 
 export const _: endPointInterface = {
 
     method: 'GET',
-    url: ['/', '/home', '/index'],
+    url: ['/projects', '/projekty', '/github'],
 
     execute: async (req, res) => {
         
-        const language = lang.pl
-        res.render('home', { socialmedia, language })
+        res.render('projects', { projects })
 
     }
 
