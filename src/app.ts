3da373a182978e6d305387e604bacf643a67b9a3
Mateusz <mateuszkdev@users.noreply.github.com>
2021-01-29 20:20:19 +0100

    fixes

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import { Application } from 'express'
 import server from './server'
 import EndpointsHandler from './private/handlers/endpointsHandler'
 import RedirectsHandler from './private/handlers/redirectsHandler'
-import { connection, redirects } from './database/handler'
+import { connection } from './database/handler'
 
 export class App {
 
