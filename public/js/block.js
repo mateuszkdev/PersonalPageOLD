6be01a1499f99e50c9b3717ce7baf51240d9f06e
Mateusz <46076250+mateuszkdev@users.noreply.github.com>
2022-07-20 19:33:02 +0200

    o

--- public/js/block.js
+++ public/js/block.js
@@ -1,3 +1,13 @@
 document.addEventListener("contextmenu", e => {
     e.preventDefault();
 }, false);
+
+const disableScroll = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+}
+
+document.addEventListener('wheel', disableScroll, {
+    passive: false
+});
